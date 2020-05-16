748ef9cbf0b10b6df78163c39162009c99fd711c
Riddy21 <songt7@mcmaster.ca>
2020-05-16 11:52:21 -0400

    initial commit

---
+++ GUI_Menu.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+#Main Page class
+class MenuGUI():
+    #Constructor  with reference to root
+    def __init__(self, window):
+        #Init variables
+        self.window = window
+
+        #Setup window
+        self.window.title("Chess")
+        self.window.geometry("500x500")
+        self.window.resizable
+            
+        #Create a frame and pack
+
+        #Create the interface and buttons
+
+    #Function for Quitting
+
+    #Function for Starting in 1 Player
+
+    #Function for Starting in 2 Player
