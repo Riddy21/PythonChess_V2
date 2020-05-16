e7678885c548573e1a9ca068610fe97d01716a32
Riddy21 <songt7@mcmaster.ca>
2020-05-16 15:03:23 -0400

    Adding menu

--- GUI_Menu.py
+++ GUI_Menu.py
@@ -10,14 +10,29 @@ class MenuGUI():
         #Setup window
         self.window.title("Chess")
         self.window.geometry("500x500")
-        self.window.resizable
-            
-        #Create a frame and pack
+        self.window.resizable(0,0)
 
-        #Create the interface and buttons
+        #Create a frame and pack with interface
+        self.titleFrame = tk.Frame(self.window)
+        self.title = tk.Label(self.titleFrame, pady=85, text="Ridvan's Chess")
+        self.oneP = tk.Button(self.titleFrame, text="One Player", padx=220, pady=50, command=self._create_1p)
+        self.twoP = tk.Button(self.titleFrame, text='Two Player', padx=220, pady=50, command=self._create_2p)
+        self.close = tk.Button(self.titleFrame, text='Close', padx=230, pady=20, command=self._quit)
+        self.title.grid(row=0)
+        self.oneP.grid(row=1)
+        self.twoP.grid(row=2)
+        self.close.grid(row=3)
+        self.titleFrame.pack()
+
 
     #Function for Quitting
+    def _quit(self):
+        self.window.quit()
 
     #Function for Starting in 1 Player
+    def _create_1p(self):
+        pass
 
     #Function for Starting in 2 Player
+    def _create_2p(self):
+        pass
