7257db3ca78af8d09658a0182243f4ce57e07d78
Riddy21 <songt7@mcmaster.ca>
2020-05-19 21:18:44 -0400

    Making chess pieces

--- Pieces.py
+++ Pieces.py
@@ -1,4 +1,22 @@
 # Abstract Piece Class
-class Piece():
-    def __init__(self):
-        self.value = ''
+class _Piece():
+    def __init__(self, value, colour, move_pattern, image):
+        # initiate variables
+        self.value = value
+        self.colour = colour
+        # TODO: Implement later
+        # self.move_pattern = move_pattern
+        self.image = image
+
+
+class Pawn(_Piece):
+    def __init__(self, colour):
+        if colour == 'white':
+            image = 'Assets/Chess_tile_pl.png'
+        elif colour == 'black':
+            image = 'Assets/Chess_tile_pd.png'
+
+        # Makes a piece with set values and images
+        super().__init__(1, colour, 'placeholder', image)
+
+
