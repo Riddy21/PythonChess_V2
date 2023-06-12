696fc90c50c5600b5c68ea9796d2896ba60eaaac
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2023-06-11 23:38:08 -0400

    old ui with tkinter

--- Main.py
+++ Main.py
@@ -1,12 +1,4 @@
-from game import Game
-from gui import ChessboardGUI
-from ai import Ai
+from game_manager import GameManager
 
 if __name__ == "__main__":
-    #GameManager()
-    game = Game()
-    ai = Ai(game, 'black')
-    gui = ChessboardGUI(game, ai)
-    #ai.start()
-
-    gui.run()
+    GameManager()
--- old/main.py
+++
@@ -1,4 +0,0 @@
-from game_manager import GameManager
-
-if __name__ == "__main__":
-    GameManager()
