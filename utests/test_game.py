3bc061dde389ac07e77e7540f262946cdef08ecd
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2023-07-01 18:35:07 -0400

    setting up and adding unittests

---
+++ utests/test_game.py
@@ -0,0 +1,14 @@
+import unittest
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
+    def test_switch_turn(self):
+        print(self.game.turn)
+        self.game.switch_turn()
+        print(self.game.turn)
+
+if __name__ == '__main__':
+    unittest.main()
